df7094465f4fea782fa401100b3433111ab0123b
hannah <3380288570@qq.com>
2026-04-25 21:59:22 +0800

    实现商品详情Redis缓存

--- services/ProductService.cc
+++ services/ProductService.cc
@@ -1,5 +1,9 @@
 #include "ProductService.h"
 #include "../dao/ProductDao.h"
+#include <hiredis/hiredis.h>
+#include <json/json.h>
+#include <sstream>
+#include <iostream>
 
 bool ProductService::createProduct(int sellerId,
                                    const std::string &name,
@@ -60,6 +64,49 @@ bool ProductService::getProductById(int productId,
         return false;
     }
 
+    std::string cacheKey = "product:" + std::to_string(productId);
+
+    // 1. 先查 Redis
+    redisContext *context = redisConnect("127.0.0.1", 6379);
+
+    if (context != nullptr && context->err == 0)
+    {
+        redisReply *reply = (redisReply *)redisCommand(context, "GET %s", cacheKey.c_str());
+
+        if (reply != nullptr && reply->type == REDIS_REPLY_STRING)
+        {
+            Json::CharReaderBuilder reader;
+            Json::Value cachedProduct;
+            std::string errs;
+            std::istringstream iss(reply->str);
+
+            if (Json::parseFromStream(reader, iss, &cachedProduct, &errs))
+            {
+                product = cachedProduct;
+                message = "get product success from redis";
+
+                freeReplyObject(reply);
+                redisFree(context);
+                return true;
+            }
+        }
+
+        if (reply != nullptr)
+        {
+            freeReplyObject(reply);
+        }
+    }
+    else
+    {
+        std::cerr << "redis connect failed" << std::endl;
+    }
+
+    if (context != nullptr)
+    {
+        redisFree(context);
+    }
+
+    // 2. Redis 没有，查 MySQL
     ProductDao productDao;
 
     if (!productDao.getProductById(productId, product))
@@ -68,6 +115,36 @@ bool ProductService::getProductById(int productId,
         return false;
     }
 
-    message = "get product success";
+    // 3. 把 MySQL 查到的数据写入 Redis，缓存 300 秒
+    redisContext *setContext = redisConnect("127.0.0.1", 6379);
+
+    if (setContext != nullptr && setContext->err == 0)
+    {
+        Json::StreamWriterBuilder writer;
+        std::string jsonStr = Json::writeString(writer, product);
+
+        redisReply *setReply = (redisReply *)redisCommand(
+            setContext,
+            "SETEX %s %d %s",
+            cacheKey.c_str(),
+            300,
+            jsonStr.c_str());
+
+        if (setReply != nullptr)
+        {
+            freeReplyObject(setReply);
+        }
+    }
+    else
+    {
+        std::cerr << "redis set connect failed" << std::endl;
+    }
+
+    if (setContext != nullptr)
+    {
+        redisFree(setContext);
+    }
+
+    message = "get product success from mysql";
     return true;
 }
